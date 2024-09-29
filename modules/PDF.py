958a11b419d92feb9b7c180a5ddc652b22518ea5
Harkirat Singh <harkirat.kirat0602@gmail.com>
2024-09-29 18:33:47 +0530

    Added Password Protection 2

--- modules/PDF.py
+++ modules/PDF.py
@@ -1,6 +1,7 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
 import os
+import PyPDF2
 
 
 def generate_pdf(folder_path:str,output:str="output.pdf",add_watermark:bool=True,custom_watermark:str=None):
@@ -33,7 +34,24 @@ def generate_pdf(folder_path:str,output:str="output.pdf",add_watermark:bool=True
     c.save()
 
 
+def password_protect(file_path,file_name,password):
+    with open(file_path+file_name, "rb") as unprotected:
+        reader = PyPDF2.PdfReader(unprotected)
+        writer = PyPDF2.PdfWriter()
+
+        for page in reader.pages:
+            writer.add_page(page)
+
+        writer.encrypt(password)
+
+        output_name = file_path+file_name.split(".")[-2]+"_protected.pdf"
+        with open(output_name,"wb") as protected:
+            writer.write(protected)
+        return output_name
+
+
 
 
 if __name__ == "__main__":
     generate_pdf("D:\Repositories\Harkirat_Image_Utilities\input",custom_watermark="Harkirat`s Image Utilities")
+    password_protect("","output.pdf","Harkirat123")
